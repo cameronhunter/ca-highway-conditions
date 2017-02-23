c4944897a7a875578c6c0236f8bc1da7524f5168
Cameron Hunter <chunter@netflix.com>
2017-02-22 18:02:05 -0800

    Colorizing

--- packages/ca-highway-conditions/src/index.js
+++ packages/ca-highway-conditions/src/index.js
@@ -1,14 +1,48 @@
 import cmdline from 'commander';
 import Parse from 'ca-highway-conditions-parser';
 import fetch from 'isomorphic-fetch';
+import chalk from 'chalk';
 
-const execute = (highways) => {
-    return fetch('http://www.dot.ca.gov/cgi-bin/roadlt').then(response => response.text()).then(text => {
-        return Parse(text);
-    }).then(conditions => {
-        console.log(conditions);
-    })
+const Color = Object.freeze({
+    highway: chalk.bold.underline,
+    title: chalk.bold,
+    text: chalk.dim
+});
+
+const empty = (highway) => ({
+    type: '',
+    highway,
+    notices: [{ title: `No notices`, messages: [] }]
+});
+
+const colorize = (conditions) => {
+    return Object.assign({
+        highway: Color.highway(`${conditions.type}${conditions.highway}`),
+        notices: conditions.notices.map((notice) => ({
+            title: Color.title(notice.title),
+            messages: notice.messages.map((message) => Color.text('• ' + message))
+        }))
+    });
 };
+
+const format = (conditions) => {
+    const notices = conditions.notices.reduce((state, notice) => [
+        ...state,
+        `${notice.title}${notice.messages.length ? '\n' : ''}${notice.messages.join('\n')}`
+    ], []);
+
+    return `${conditions.highway}\n${notices.join('\n')}`;
+};
+
+const execute = (highways) => (
+    fetch('http://www.dot.ca.gov/cgi-bin/roadlt')
+        .then(response => response.text())
+        .then(Parse)
+        .then(conditions => highways.reduce((state, highway) => [...state, conditions[highway] || empty(highway)].filter(Boolean), {}))
+        .then(conditions => conditions.map(colorize))
+        .then(conditions => conditions.map(format))
+        .then(conditions => console.log(conditions.join('\n\n')))
+);
 
 cmdline
     .arguments('<highway...>')
